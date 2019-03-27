567d19ed3805876a149740a1d10f568991b88a4a
andyang <yang.bng86@gmail.com>
2019-03-27 20:38:02 +0800

    Nathan

--- app/src/main/java/andynag/tw/tennis_coding_dojo_20190327/TennisGame.kt
+++ app/src/main/java/andynag/tw/tennis_coding_dojo_20190327/TennisGame.kt
@@ -2,14 +2,28 @@ package andynag.tw.tennis_coding_dojo_20190327
 
 class TennisGame(player1: String, player2: String) {
 
-    var player1Score = 1
-    var player2Score = 0
+    var player1Score :Int
+    var player2Score :Int
+    init {
+        player1Score = 0
+        player2Score = 0
+    }
 
     fun getResult(): String {
+        var player1ScoreTag = "Love"
+        var player2ScoreTag = "Love"
+
+        when(player1Score) {
+            0 -> player1ScoreTag = "Love"
+            1 -> player1ScoreTag = "Fifteen"
+        }
+
         if (player1Score == player2Score) {
             return "Love All"
         } else if (player1Score == 1 && player2Score == 0) {
             return "Fifteen/Love"
+        } else if (player1Score == 0 && player2Score == 1) {
+            return "Love/Fifteen"
         } else {
             return ""
         }
