683e0ce3802aebc1e654961dbb7817f8b35d21de
andyang <yang.bng86@gmail.com>
2019-03-27 20:45:52 +0800

    Hermes

--- app/src/main/java/andynag/tw/tennis_coding_dojo_20190327/TennisGame.kt
+++ app/src/main/java/andynag/tw/tennis_coding_dojo_20190327/TennisGame.kt
@@ -2,30 +2,37 @@ package andynag.tw.tennis_coding_dojo_20190327
 
 class TennisGame(player1: String, player2: String) {
 
-    var player1Score :Int
-    var player2Score :Int
+    var player1Score: Int
+    var player2Score: Int
+
     init {
         player1Score = 0
         player2Score = 0
     }
 
     fun getResult(): String {
-        var player1ScoreTag = "Love"
-        var player2ScoreTag = "Love"
-
-        when(player1Score) {
-            0 -> player1ScoreTag = "Love"
-            1 -> player1ScoreTag = "Fifteen"
-        }
+        var player1ScoreTag = getScopeString(player1Score)
+        var player2ScoreTag = getScopeString(player2Score)
 
         if (player1Score == player2Score) {
-            return "Love All"
+            return "$player1ScoreTag All"
         } else if (player1Score == 1 && player2Score == 0) {
-            return "Fifteen/Love"
+            return "$player1ScoreTag/$player2ScoreTag"
         } else if (player1Score == 0 && player2Score == 1) {
-            return "Love/Fifteen"
+            return "$player1ScoreTag/$player2ScoreTag"
+        } else if (player1Score == 2 && player2Score == 0) {
+            return "$player1ScoreTag/$player2ScoreTag"
         } else {
             return ""
+        }
+    }
+
+    private fun getScopeString(scope: Int): String {
+        return when (scope) {
+            0 -> "Love"
+            1 -> "Fifteen"
+            2 -> "Thirty"
+            else -> ""
         }
     }
 
--- app/src/test/java/andynag/tw/tennis_coding_dojo_20190327/TennisGameTest.kt
+++ app/src/test/java/andynag/tw/tennis_coding_dojo_20190327/TennisGameTest.kt
@@ -3,7 +3,7 @@ package andynag.tw.tennis_coding_dojo_20190327
 import org.junit.Assert
 import org.junit.Test
 
-class TennisGameTest{
+class TennisGameTest {
 
     @Test
     fun test_love_all() {
@@ -31,5 +31,14 @@ class TennisGameTest{
         game.player2Score = 1
         val actual = game.getResult()
         Assert.assertEquals("Love/Fifteen", actual)
+    }
+
+    @Test
+    fun test_thirty_love() {
+        val game = TennisGame("player1", "player2")
+        game.player1Score = 2
+        game.player2Score = 0
+        val actual = game.getResult()
+        Assert.assertEquals("Thirty/Love", actual)
     }
 }
